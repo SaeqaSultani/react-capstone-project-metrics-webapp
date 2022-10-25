889d4b7dbcbc6332943687963aaeb8420025fb00
Saeqa Sultani <saeqasultani@gmail.com>
2022-10-25 19:04:28 +0430

    Solved the api issue

--- src/redux/companies-list.js
+++ src/redux/companies-list.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const GET_COMPANIES = 'companies/companies/GET_COMPANIES';
 const GET_COMPANIEY_DETAILS = 'companies/companies/GET_COMPANIEY_DETAILS';
 
-const url = 'https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=6f7d6f7fb9d6628c3bcdc2ea4a338d9f';
+const url = 'https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=55b862a869deb41e6274437272a2c936';
 
 const initialState = { companyList: [], companyDetails: {} };
 
@@ -38,8 +38,9 @@ export const getCompanies = createAsyncThunk(
 export const getCompanyDetails = createAsyncThunk(
   GET_COMPANIEY_DETAILS,
   async (args, { dispatch }) => {
-    const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${args}?apikey=6f7d6f7fb9d6628c3bcdc2ea4a338d9f`);
+    const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${args}?apikey=55b862a869deb41e6274437272a2c936`);
     const data = await response.json();
+    // console.log(`data: ${data}`);
 
     dispatch({ type: GET_COMPANIEY_DETAILS, companies: data[0] });
   },
